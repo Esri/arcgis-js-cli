fd22488314953eab1e48d86c370c675ca92daeab
City of Vernonia GIS <gis@vernonia-or.gov>
2020-01-27 08:31:25 -0800

    Add `view` property and constructor interface to widget template

--- templates/basic/widget/src/widgets/WidgetName.tsx
+++ templates/basic/widget/src/widgets/WidgetName.tsx
@@ -1,14 +1,22 @@
+import esri = __esri;
+
 import {
   aliasOf,
   declared,
   property,
   subclass
 } from "esri/core/accessorSupport/decorators";
+
 import { renderable, tsx } from "esri/widgets/support/widget";
 
 import Widget from "esri/widgets/Widget";
 
 import <%name%>ViewModel from "./<%name%>/<%name%>ViewModel";
+
+export interface <%name>Properties extends esri.WidgetProperties {
+  name?: string;
+  view?: esri.MapView | esri.SceneView;
+}
 
 const CSS = {
   base: "esri-widget <%name-lower%>-base"
@@ -16,6 +24,9 @@ const CSS = {
 
 @subclass("app.widgets.<%name%>")
 export default class <%name%> extends declared(Widget) {
+
+  @aliasOf("viewModel.view")
+  view: esri.MapView | esri.SceneView;
 
   @aliasOf("viewModel.name")
   @renderable()
@@ -26,6 +37,10 @@ export default class <%name%> extends declared(Widget) {
   })
   @renderable()
   viewModel: <%name%>ViewModel = new <%name%>ViewModel();
+
+  constructor(properties?: <%name>Properties) {
+    super();
+  }
 
   render() {
     return (
